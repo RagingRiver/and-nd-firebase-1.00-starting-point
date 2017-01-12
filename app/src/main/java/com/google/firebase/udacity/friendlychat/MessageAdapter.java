8172762356f1d00c9c97d62a339098e638c98f7c
Gregory <greg@pocketmatter.com>
2017-01-11 20:35:46 -0600

    This is a minor change test of GitHub

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MessageAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
     public MessageAdapter(Context context, int resource, List<FriendlyMessage> objects) {
+        // sample test
         super(context, resource, objects);
     }
 
